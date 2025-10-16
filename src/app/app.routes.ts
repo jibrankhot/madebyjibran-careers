1c9f60786e5a40a0355c3dd8efa1073ef050a28f
jibran <jibrankhot5@gmail.com>
2025-10-16 22:17:53 +0530

    feat: setup base routing, navbar, and home page layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { JobListComponent } from './pages/jobs/job-list/job-list.component';
+import { SettingsComponent } from './pages/settings/settings/settings.component';
+import { RssFeedManagerComponent } from './rss-feed-manager/rss-feed-manager.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'feeds', component: RssFeedManagerComponent },
+    { path: 'jobs', component: JobListComponent },
+    { path: 'settings', component: SettingsComponent },
+    { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/rss-feed-manager/rss-feed-manager.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rss-feed-manager-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './rss-feed-manager.component.html',
+  styleUrls: ['./rss-feed-manager.component.scss']
+})
+export class RssFeedManagerComponent {
+
+}
